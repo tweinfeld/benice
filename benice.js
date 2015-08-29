9755310120e06ac51bebef9329157c86db15124d
Tal Weinfeld <talw@webwise.co.il>
2015-08-30 01:05:34 +0300

    Minor names refactor Extra tweaking parameters added to "onIdle"

--- benice.js
+++ benice.js
@@ -3,24 +3,25 @@
         { detect: function(){ return !!((typeof define === "function") && define.amd); }, register: function(Tools){ define(Tools); } },
         { detect: function(){ return !!(typeof exports !== "undefined"); }, register: function(Tools){ module.exports = Tools; } },
         { detect: function(){ return true; }, register: function(Tools){ window.Benice = Tools; } }
-    ].filter(function(o){ return o.detect(); }).shift().register(Tools);
-})((function(undefined, window){
+    ].filter(function(){ return !!window; }).filter(function(o){ return o.detect(); }).splice(0,1).forEach(function(o){ o.register(Tools(window)); });
+})(function(window){
 
-    var	DEFAULT_INTERVAL = 150,
+    var	DEFAULT_SAMPLE_INTERVAL = 150,
         IDLE_THRESHOLD = 50,
-        RESOLUTION_SAMPLES = 5;
+        RESOLUTION_SAMPLES = 5,
+        DETECTION_EXPIRATION = 60000;
 
     var setTimeout = window.setTimeout,
         clearInterval = window.clearInterval,
         now = Date.now.bind(Date);
 
-    var Monitor = function(listener, interval){
-        var interval = interval || DEFAULT_INTERVAL,
+    var Monitor = function(listener, sampleInterval){
+        var sampleInterval = sampleInterval || DEFAULT_SAMPLE_INTERVAL,
             timer;
 
         (function sampler(prev){
-            timer = setTimeout(sampler, interval, now());
-            prev && listener(now() - prev - interval);
+            timer = setTimeout(sampler, sampleInterval, now());
+            prev && listener(now() - prev - sampleInterval);
         })();
 
         return function(){
@@ -31,14 +32,19 @@
     var onIdle = function(){
         var args = Array.prototype.slice.call(arguments),
             func = args.shift(),
+            expiration = args.shift() || DETECTION_EXPIRATION,
+            resolutionSamples = args.shift() || RESOLUTION_SAMPLES,
+            sampleInterval = args.shift() || DEFAULT_SAMPLE_INTERVAL,
+            sampleThreshold = args.shift() || IDLE_THRESHOLD,
             samplesBuffer = [],
+            detectionStart = now(),
             stopMonitoring = Monitor(
                 function(sample){
-                    (samplesBuffer = samplesBuffer.concat([Math.abs(sample)])
-                        .splice(-RESOLUTION_SAMPLES, RESOLUTION_SAMPLES))
-                        .filter(function(sample){ return sample < IDLE_THRESHOLD; })
-                        .length === RESOLUTION_SAMPLES && stopMonitoring(func.apply(undefined, args));
-                }
+                    ((samplesBuffer = samplesBuffer.concat([Math.abs(sample)])
+                        .splice(-resolutionSamples, resolutionSamples))
+                        .filter(function(sample){ return sample < sampleThreshold; })
+                        .length === resolutionSamples || (now() - detectionStart) > expiration) && stopMonitoring(func.apply(undefined, args));
+                }, sampleInterval
             );
 
         return stopMonitoring;
@@ -48,5 +54,4 @@
         "Monitor": Monitor,
         "onIdle": onIdle
     }
-
-})(undefined, window));
+});
